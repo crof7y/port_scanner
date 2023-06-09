5c6ad3c414c97f97886457e8e026368ce622fccc
Mike Crofts <120604635+crof7y@users.noreply.github.com>
2023-06-09 10:16:31 -0500

    Update scan-linux.py
    Added in Comments for clarity

--- scan-linux.py
+++ scan-linux.py
@@ -3,6 +3,8 @@
 import nmap
 import time
 import os
+
+#output text formatting
 bstart = '\033[1m'
 gstart = '\033[2m'
 red = '\033[91m'
@@ -11,20 +13,31 @@ uline = '\033[4m'
 fstop = '\033[0;0m'
 purple = '\033[95m'
 
+#Header Info
 print(bstart + red + uline + '\n\tWelcome to The Vulnerability Assessment Port Scanner' + fstop)
 print(gstart + '\tInput Host Addresses in the following format: 192.168.1.150 (example)' + fstop)
 print(gstart + '\tInput Network Addresses in the following format: 192.168.1.1/24 (example)' + fstop)
 time.sleep(1)
+
+#Initialize new Port Scanner
 scan = nmap.PortScanner()
+
+#Ask for user input
 addressID = input(bstart + '\n\tInput Address to Scan: ' + fstop)
 print(gstart + '\n\tInitializing Scan...' + fstop)
 time.sleep(2)
 print(gstart + '\n\tScanning.....This may take some time.' + fstop)
+
+#Scan Network
 scan.scan(addressID, arguments = '-A -T4 -p 21-443,1433-1434')
+
+#Output Results
 for host in scan.all_hosts():
     print('\n\t\t------------------------------------------------------------------------------------------')
     print(blue + bstart + '\t\tHost: ' + fstop + '{0} ({1})'.format(host, scan[host].hostname()))
     print(blue + bstart + '\t\tState: ' + fstop + '{0}'.format(scan[host].state()))
+
+    #Operating System Matching
     #if scan[host]['osmatch']:
     #    if scan[host]['osmatch'][0]['osclass'][0]['vendor'] == 'Microsoft':
     #        os = 'Windows'
@@ -35,17 +48,20 @@ for host in scan.all_hosts():
     #else:
     #    print(blue + bstart +'\t\tOS: ' +fstop + 'Unknown')
 
+    #Output Port Protocol
     for protocol in scan[host].all_protocols():
         print(blue + bstart + '\t\tProtocol: ' + fstop + '{0}'.format(protocol))
-
         lport = scan[host][protocol].keys()
         sorted(lport)
+        
+        #Port Identification and Output
         for port in lport:
             if port == 21 or port == 22 or port == 23 or port == 25 or port == 53 or port == 110 or port == 135 or port == 137 or port == 139 or port == 443 or port == 1433 or port == 1434:
                 print(red+bstart+'\t\t\tport: '+str(port)+red+bstart+'\tstate: {0}'.format(scan[host][protocol][port]['state'])+fstop)
             else:
                 print(purple+bstart+'\t\t\tport: '+str(port)+'\tstate: {0}'.format(scan[host][protocol][port]['state'])+fstop)
 
+        #Printing Out Recommendations for closing open ports
         print('\n')
         print(bstart + uline + '\t\t\tRecommended Actions:' +fstop+gstart+ ' close all open ports.' +fstop)
         print(bstart + uline + '\t\t\tTo Close Open Ports:' +fstop+gstart+ ' access firewall and create rules to close ports.' +fstop)
@@ -54,6 +70,7 @@ for host in scan.all_hosts():
             print(gstart + '\t\t\tTo minimize vulnerability, ' +fstop+bstart+uline+red+ 'CLOSE THESE PORTS IMMEDIATELY:' +fstop)
         print('\n')
 
+        #Information on ports that *should* be closed
         for port in lport:
             if port == 21:
                 print(bstart + red + '\t\t\tPort 21: ' +fstop+gstart+ 'FTP' + fstop)
